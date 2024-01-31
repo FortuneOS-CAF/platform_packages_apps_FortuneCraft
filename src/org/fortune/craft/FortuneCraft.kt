d060245dc66450aebdff7802761cc73470d671a8
xyyx <xyyx@mail.ru>
2024-01-31 11:15:42 +0100

    Craft: Initialize
    Co-authored-by: madmax7896 <madmax78968@gmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Kizziama <kizziama@proton.me>
    Co-authored-by: Mikazuuu07 <mikazuuu07@komodo-os.my.id>
    Change-Id: Ic1fae5714198f7b70bc8ac3ed1b878698e74fc04

---
+++ src/org/fortune/craft/FortuneCraft.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2024-2025 FortuneOS
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.fortune.craft
+
+import com.android.internal.logging.nano.MetricsProto
+import com.android.settings.dashboard.DashboardFragment
+import com.android.settings.R
+import com.android.settings.search.BaseSearchIndexProvider
+import com.android.settingslib.search.SearchIndexable
+
+@SearchIndexable
+class FortuneCraft : DashboardFragment() {
+
+    override fun getPreferenceScreenResId(): Int {
+        return R.xml.craft
+    }
+
+    override fun getMetricsCategory(): Int {
+        return MetricsProto.MetricsEvent.FORTUNE
+    }
+
+    override fun getLogTag(): String {
+        return TAG
+    }
+
+    companion object {
+        private const val TAG = "FortuneCraft"
+
+        @JvmField
+        val SEARCH_INDEX_DATA_PROVIDER = BaseSearchIndexProvider(R.xml.craft)
+    }
+}
