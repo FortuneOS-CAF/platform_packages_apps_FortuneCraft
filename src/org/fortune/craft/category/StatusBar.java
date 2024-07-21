c795fb3181c7d2e69fb411e3fbd14b47e104be48
Kizziama <kizziama@proton.me>
2024-07-21 05:11:32 +0000

    Revert "Craft: Add ability to toggle bluetooth battery level [2/2]"
    This reverts commit 4b9523ec30b8f44e859217d3e02ab6dddcba7e09.
    
    Change-Id: I8f09a1a53811ad68fe29299ff42fbfa00e8ccbec

--- src/org/fortune/craft/category/StatusBar.java
+++ src/org/fortune/craft/category/StatusBar.java
@@ -47,13 +47,6 @@ public class StatusBar extends SettingsPreferenceFragment implements
         return false;
     }
 
-    public static void reset(Context mContext) {
-        ContentResolver resolver = mContext.getContentResolver();
-
-        Settings.System.putIntForUser(resolver,
-                Settings.System.BLUETOOTH_SHOW_BATTERY, 1, UserHandle.USER_CURRENT);
-    }
-
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.FORTUNE;
