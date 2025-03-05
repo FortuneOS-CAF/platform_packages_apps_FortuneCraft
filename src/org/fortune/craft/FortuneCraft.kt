3f129f8e4548405505391fc68bddf451be481b7f
Mikazuuu07 <mikazuuu07@komodo-os.my.id>
2025-03-06 01:23:17 +0700

    Craft: Migrate `FortuneCraft.java` to kotlin
    Signed-off-by: Mikazuuu07 <mikazuuu07@komodo-os.my.id>

---
+++ src/org/fortune/craft/FortuneCraft.kt
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2024-2025 FortuneOS
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.fortune.craft
+
+import android.content.ContentResolver
+import android.content.Context
+import android.content.res.Resources
+import android.os.Bundle
+import android.provider.SearchIndexableResource
+import android.provider.Settings
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.android.internal.logging.nano.MetricsProto
+import com.android.settings.R
+import com.android.settings.SettingsPreferenceFragment
+import com.android.settings.search.BaseSearchIndexProvider
+import com.android.settingslib.search.Indexable
+import com.android.settingslib.search.SearchIndexable
+import org.fortune.framework.preferences.SystemSettingMasterSwitchPreference
+
+@SearchIndexable
+class FortuneCraft : SettingsPreferenceFragment(), Preference.OnPreferenceChangeListener, Indexable {
+
+    private val NETWORK_TRAFFIC_STATE = "network_traffic_state"
+
+    private var mNetTrafficState: SystemSettingMasterSwitchPreference? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val resolver: ContentResolver = requireActivity().contentResolver
+        addPreferencesFromResource(R.xml.craft)
+
+        mNetTrafficState = findPreference(NETWORK_TRAFFIC_STATE)
+        mNetTrafficState?.setOnPreferenceChangeListener(this)
+        val enabled = Settings.System.getInt(resolver, NETWORK_TRAFFIC_STATE, 0) == 1
+        mNetTrafficState?.isChecked = enabled
+        updateNetTrafficSummary(enabled)
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+        val resolver: ContentResolver = requireActivity().contentResolver
+        if (preference == mNetTrafficState) {
+            val enabled = newValue as Boolean
+            Settings.System.putInt(resolver, NETWORK_TRAFFIC_STATE, if (enabled) 1 else 0)
+            updateNetTrafficSummary(enabled)
+            return true
+        }
+        return false
+    }
+
+    private fun updateNetTrafficSummary() {
+        val enabled = Settings.System.getInt(
+            requireActivity().contentResolver, NETWORK_TRAFFIC_STATE, 0
+        ) == 1
+        updateNetTrafficSummary(enabled)
+    }
+
+    private fun updateNetTrafficSummary(enabled: Boolean) {
+        if (mNetTrafficState == null) return
+        var summary = requireActivity().getString(R.string.switch_off_text)
+        if (enabled) {
+            val status = Settings.System.getInt(
+                requireActivity().contentResolver,
+                Settings.System.NETWORK_TRAFFIC_VIEW_LOCATION, 0
+            )
+            val resid = when (status) {
+                1 -> R.string.traffic_expanded_statusbar
+                2 -> R.string.show_network_traffic_all
+                else -> R.string.traffic_statusbar
+            }
+            summary = requireActivity().getString(R.string.network_traffic_state_summary) + " " +
+                    requireActivity().getString(resid)
+        }
+        mNetTrafficState?.summary = summary
+    }
+
+    private fun updateNetTrafficValue() {
+        if (mNetTrafficState == null) return
+        val enabled = Settings.System.getInt(
+            requireActivity().contentResolver, NETWORK_TRAFFIC_STATE, 0
+        ) == 1
+        mNetTrafficState?.isChecked = enabled
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateNetTrafficSummary()
+        updateNetTrafficValue()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun getMetricsCategory(): Int {
+        return MetricsProto.MetricsEvent.FORTUNE
+    }
+
+    companion object {
+        @JvmField
+        val SEARCH_INDEX_DATA_PROVIDER = object : BaseSearchIndexProvider() {
+            override fun getXmlResourcesToIndex(context: Context, enabled: Boolean): List<SearchIndexableResource> {
+                val result = ArrayList<SearchIndexableResource>()
+                val sir = SearchIndexableResource(context)
+                sir.xmlResId = R.xml.craft
+                result.add(sir)
+                return result
+            }
+
+            override fun getNonIndexableKeys(context: Context): List<String> {
+                return super.getNonIndexableKeys(context).apply {
+                    add(NETWORK_TRAFFIC_STATE)
+                }
+            }
+        }
+    }
+}
