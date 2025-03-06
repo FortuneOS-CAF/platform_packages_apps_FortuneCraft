50f54a715641a765b44799a34e33482054c942a6
Kizziama <kizziama@proton.me>
2025-03-06 07:58:05 +0000

    Craft: Migrate FortuneCraft to `DashboardFragment`
    Change-Id: I743a8a056aef68f8083c3dad58ccf51819d62074

--- src/org/fortune/craft/FortuneCraft.kt
+++ src/org/fortune/craft/FortuneCraft.kt
@@ -5,43 +5,48 @@
 
 package org.fortune.craft
 
-import android.content.ContentResolver
 import android.content.Context
 import android.content.res.Resources
 import android.os.Bundle
 import android.provider.SearchIndexableResource
 import android.provider.Settings
 import androidx.preference.Preference
-import androidx.preference.PreferenceFragmentCompat
 import com.android.internal.logging.nano.MetricsProto
 import com.android.settings.R
-import com.android.settings.SettingsPreferenceFragment
+import com.android.settings.dashboard.DashboardFragment
 import com.android.settings.search.BaseSearchIndexProvider
 import com.android.settingslib.search.Indexable
 import com.android.settingslib.search.SearchIndexable
 import org.fortune.framework.preferences.SystemSettingMasterSwitchPreference
 
 @SearchIndexable
-class FortuneCraft : SettingsPreferenceFragment(), Preference.OnPreferenceChangeListener, Indexable {
+class FortuneCraft : DashboardFragment(), Preference.OnPreferenceChangeListener, Indexable {
 
     private val NETWORK_TRAFFIC_STATE = "network_traffic_state"
-
     private var mNetTrafficState: SystemSettingMasterSwitchPreference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val resolver: ContentResolver = requireActivity().contentResolver
-        addPreferencesFromResource(R.xml.craft)
+        updatePreferences()
+    }
+
+    override fun getPreferenceScreenResId(): Int = R.xml.craft
+
+    override protected val logTag: String = "FortuneCraft"
+
+    private fun updatePreferences() {
+        val resolver = requireActivity().contentResolver
 
         mNetTrafficState = findPreference(NETWORK_TRAFFIC_STATE)
-        mNetTrafficState?.setOnPreferenceChangeListener(this)
-        val enabled = Settings.System.getInt(resolver, NETWORK_TRAFFIC_STATE, 0) == 1
-        mNetTrafficState?.isChecked = enabled
-        updateNetTrafficSummary(enabled)
+        mNetTrafficState?.apply {
+            setOnPreferenceChangeListener(this@FortuneCraft)
+            isChecked = Settings.System.getInt(resolver, NETWORK_TRAFFIC_STATE, 0) == 1
+            updateNetTrafficSummary(isChecked)
+        }
     }
 
     override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
-        val resolver: ContentResolver = requireActivity().contentResolver
+        val resolver = requireActivity().contentResolver
         if (preference == mNetTrafficState) {
             val enabled = newValue as Boolean
             Settings.System.putInt(resolver, NETWORK_TRAFFIC_STATE, if (enabled) 1 else 0)
@@ -51,15 +56,11 @@ class FortuneCraft : SettingsPreferenceFragment(), Preference.OnPreferenceChange
         return false
     }
 
-    private fun updateNetTrafficSummary() {
-        val enabled = Settings.System.getInt(
-            requireActivity().contentResolver, NETWORK_TRAFFIC_STATE, 0
-        ) == 1
-        updateNetTrafficSummary(enabled)
-    }
+    private fun updateNetTrafficSummary(enabled: Boolean = Settings.System.getInt(
+        requireActivity().contentResolver, NETWORK_TRAFFIC_STATE, 0) == 1) {
 
-    private fun updateNetTrafficSummary(enabled: Boolean) {
         if (mNetTrafficState == null) return
+
         var summary = requireActivity().getString(R.string.switch_off_text)
         if (enabled) {
             val status = Settings.System.getInt(
@@ -77,42 +78,25 @@ class FortuneCraft : SettingsPreferenceFragment(), Preference.OnPreferenceChange
         mNetTrafficState?.summary = summary
     }
 
-    private fun updateNetTrafficValue() {
-        if (mNetTrafficState == null) return
-        val enabled = Settings.System.getInt(
-            requireActivity().contentResolver, NETWORK_TRAFFIC_STATE, 0
-        ) == 1
-        mNetTrafficState?.isChecked = enabled
-    }
-
     override fun onResume() {
         super.onResume()
         updateNetTrafficSummary()
-        updateNetTrafficValue()
     }
 
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun getMetricsCategory(): Int {
-        return MetricsProto.MetricsEvent.FORTUNE
-    }
+    override fun getMetricsCategory(): Int = MetricsProto.MetricsEvent.FORTUNE
 
     companion object {
         @JvmField
         val SEARCH_INDEX_DATA_PROVIDER = object : BaseSearchIndexProvider() {
             override fun getXmlResourcesToIndex(context: Context, enabled: Boolean): List<SearchIndexableResource> {
-                val result = ArrayList<SearchIndexableResource>()
-                val sir = SearchIndexableResource(context)
-                sir.xmlResId = R.xml.craft
-                result.add(sir)
-                return result
+                return listOf(SearchIndexableResource(context).apply {
+                    xmlResId = R.xml.craft
+                })
             }
 
             override fun getNonIndexableKeys(context: Context): List<String> {
                 return super.getNonIndexableKeys(context).apply {
-                    add(NETWORK_TRAFFIC_STATE)
+                    add("network_traffic_state")
                 }
             }
         }
